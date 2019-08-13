91079d997d1abbd735ebdd9e814c29f51f13bf11
rezaeerad <30417782+rezaeerad@users.noreply.github.com>
2019-08-13 23:19:57 +0430

    Add files via upload

---
+++ get_sepsis_score.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+from sklearn.externals import joblib
+import numpy as np
+import pandas as pd
+
+def get_sepsis_score(data, model):
+    num_rows = len(data)
+
+    meanF = np.load('meanF.npy')
+    M1 = joblib.load('model-saved.pkl')
+
+    ####### Impute
+    imputePatient = []
+    for interval in range(data.shape[0]):      #### loop for on intervals
+        if interval == 0:
+            newData = np.copy(data[0,:])
+            for column in range(40):       ########  loop for on columns
+                if (np.isnan(newData[column])):
+                    newData[column] = meanF[column]
+            # imputePatient.append(newData)
+            imputePatient = newData
+
+        else:
+            index = np.arange(interval+1)
+            aa = np.copy(data[index])
+            aa[0, :] = newData
+            df = pd.DataFrame.from_records(aa)
+            df.interpolate(method='linear', inplace=True)
+            newData1 = np.array(df)
+            # imputePatient.append(newData1[-1, :])
+            imputePatient = np.vstack((imputePatient, newData1[-1, :]))
+
+    data = imputePatient
+    ####### End Impute
+
+    predicted = M1.predict(data)
+
+    # ###################################
+
+    # return score_final, label_final
+    if predicted[num_rows - 1] == 0:
+        score = 0.4
+    else:
+        score = 0.6
+
+    label = predicted[num_rows-1]
+    return score, label
+
+def load_sepsis_model():
+
+    return None
